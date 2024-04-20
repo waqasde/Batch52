f5bf63d5f552c462d37a048f0d5c1033e775aa78
waqas ahmad <waqasde@yahoo.com>
2024-04-21 04:31:07 +0500

    updates

--- Q2/my-nextjs-routers-ass1-project/src/app/about/page.tsx
+++ Q2/my-nextjs-routers-ass1-project/src/app/about/page.tsx
@@ -6,7 +6,7 @@ const About: React.FC = () => {
     const router = useRouter()
     return (
         
-        <MainLayout>
+       
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                     <h1>this is About Page</h1>
@@ -16,7 +16,7 @@ const About: React.FC = () => {
                 </div>
               
                 </main>
-        </MainLayout>
+       
     );
 };
 
--- Q2/my-nextjs-routers-ass1-project/src/app/contact/page.tsx
+++ Q2/my-nextjs-routers-ass1-project/src/app/contact/page.tsx
@@ -1,15 +1,20 @@
+'use client'
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
-
+import { useRouter } from 'next/navigation'
 const Contact: React.FC = () => {
+    const router = useRouter()
     return (
-        <MainLayout>
+       
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                    <h1>this is Contact US Page</h1>
+                <h1>this is Contact US Page</h1>
+                <button type="button" onClick={() => router.push('/')}>
+                    Back to home
+                </button>
                 </div>
                 </main>
-        </MainLayout>
+        
     );
 };
 
